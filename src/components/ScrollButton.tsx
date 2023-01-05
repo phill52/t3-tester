2848031298f694fa7846233070fb3fc980eeaf84
Phillip Anerine <paner225@gmail.com>
2023-01-05 17:43:54 -0500

    progress on scrolling pages

---
+++ src/components/ScrollButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/componentstyles.css';
+
+const ScrollButton = () => {
+
+  return (
+    <div>
+      <button className='scrollButton'></button>
+    </div>
+  )
+}
+
+export default ScrollButton
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
 import styles from "./index.module.css";
+import React, {useState} from 'react';
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import {motion,useScroll,useSpring,useTransform,MotionValue} from "framer-motion";
+import { useRef } from "react";
+import { render } from "react-dom";
+
+const useParallax = (value: MotionValue<number>, distance: number) =>{
+  return useTransform(value, [0, 1], [-distance, distance]);
+}  
+// const renderPage: NextPage = (props) =>{
+//   const ref = useRef(null);
+//   const { scrollYProgress } = useScroll({ target: ref });
+//   const y = useParallax(scrollYProgress, 300);
+//   return (
+//     <section className={`container homepage${props.id}`}>
+//       <div ref={ref}>
+//         {props.page}
+//       </div>
+//       <motion.h2 style={{ y }}>{`#00${props.id}`}</motion.h2>
+//     </section>
+    
+//     );
+// }
 
 const Home: NextPage = () => {
+
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+  const handleScroll = (itemId: string) => {
+    const element=document.getElementById(itemId);
+    if (element) element.scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
     <>
       <Head>
@@ -13,11 +48,39 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.container}>
-          <h1 className={styles.title}>
-            Phillip <span className={styles.whiteSpan}>Anerine</span>
-          </h1>
-        </div>
+        {/* {pages.map((page)=>
+        <renderPage page={page} />)} */}
+        <div className={styles.fullPageContainer}>
+
+          <section className={styles.container}>
+            <div>
+            <h1 className={styles.title}>
+              Phillip <span className={styles.whiteSpan}>Anerine</span>
+            </h1>
+            <img src={'/images/downarrow.png'} className={styles.downarrow}/>
+            <button onClick={()=>handleScroll('section-2')}>Hello</button>
+            </div>
+          </section>
+          
+
+          <section className={styles.container} id='section-2'>
+            <div>
+            <h2 className={styles.title}>About me</h2>
+            <br></br>
+            <div className={styles.profileContainer}>
+              <img src={'/images/panerine.jpg'} className={styles.headshot} alt="Headshot of Phillip Anerine"/>
+              <p className={styles.bio}>Hello! I am a 3/4 Computer Science Major studying at Stevens Institute of Technology.
+                    I'm from Garfield, NJ, and I'm an aspiring Software Engineer with lots of experience in web development, working
+                    on both front-end and back-end. I am a highly motivated self-starter, and am always looking for new opportunities 
+                    to gain new experiences. You can look at some of my projects <a> here. </a>
+                    </p>
+              </div>
+            </div>
+          </section>
+      
+      </div>
+
+      <motion.div className="progress" style={{ scaleX }} />
       </main>
     </>
   );
